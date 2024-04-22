011b6ddb218a36558c81077dddf5c6076e6e0bd0
RachelG <rachelmarie.gangemi@gmail.com>
2024-04-22 14:59:55 -0400

    add file

---
+++ js/app.js
@@ -0,0 +1,20 @@
+function $(cssSelector) {
+    return document.querySelector(cssSelector)
+}
+
+console.log('testing');
+
+/*-------------- Constants -------------*/
+
+
+/*---------- Variables (state) ---------*/
+
+
+/*----- Cached Element References  -----*/
+
+
+/*-------------- Functions -------------*/
+
+
+/*----------- Event Listeners ----------*/
+
